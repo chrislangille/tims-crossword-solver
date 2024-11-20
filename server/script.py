e23ab34d52b38e7586f0bb4ca29f61394b131dbd
theobourgeois <theoobourgeois@gmail.com>
2024-11-20 10:33:05 -0400

    add letter detection

--- server/script.py
+++ server/script.py
@@ -1,6 +1,8 @@
 from PIL import Image, ImageChops
 import numpy
 
+letter_dimensions = (78, 82)
+
 #only keep the white pixels
 def whites_only(bg):
     # Create a mask for the white pixels
@@ -14,17 +16,32 @@ def whites_only(bg):
 
 def find_letter_bounds(img):
     origin = None
+    origin_row = None
+    origin_col = None
 
-    # find top left corner
-    for row in range(0, mask_dimensions[0]):
-        for col in range(0, mask_dimensions[1]):
+    for col in range(0, mask_dimensions[1]):
+        for row in range(0, mask_dimensions[0]):
             if img[row][col][0] != 0:
-                origin = (row, col)
+                origin_col = col
                 break
-        if origin != None:
+        if origin_col != None:
             break
 
-    return origin, 80, 80
+    if(origin_col == None):
+        return (None, None), 0, 0
+
+    for row in range(0, mask_dimensions[0]):
+        for col in range(origin_col, origin_col + letter_dimensions[1]):
+            if img[row][col][0] != 0:
+                origin_row = row
+                break
+        if origin_row != None:
+            break
+
+    origin = (origin_row, origin_col)
+
+
+    return origin, *letter_dimensions
     
 
 # Load images
@@ -41,12 +58,16 @@ ss_array = numpy.array(ss).astype(float)
 filtered_letters = whites_only(ss_array)
 
 origin, width, height = find_letter_bounds(filtered_letters)
-print(origin, width, height)
+i = 0
+while origin[0] != None:    
+    letter = Image.fromarray(numpy.uint8(filtered_letters))
+    letter = letter.crop((origin[1], origin[0], origin[1] + width, origin[0] + height))
+    letter.save('assets/letter' + str(i) + '.png')
 
-# create image of letter_dimensions
-letter = Image.fromarray(numpy.uint8(filtered_letters))
-letter =  letter.crop((origin[1], origin[0], origin[1] + width, origin[0] + height))
-letter.save('assets/letter.png')
+    filtered_letters[origin[0]:origin[0] + height, origin[1]:origin[1] + width] = 0
+    origin, width, height = find_letter_bounds(filtered_letters)
+    i += 1
+
 
 
 # Convert the result back to a PIL Image and save
